e07271f5dc41a5351930f9bfa03505ab1e2763e9
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-10-05 10:25:39 +0200

    Create GeotimewfsApplicationTests.java

---
+++ src/test/java/info/ponciano/lab/geotimewfs/GeotimewfsApplicationTests.java
@@ -0,0 +1,13 @@
+package info.ponciano.lab.geotimewfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeotimewfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
