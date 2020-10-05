1069cadd56b7e12bb53a5551686f7816f212f4b2
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-10-05 17:56:50 +0200

    Create QueryinterfaceController.java

---
+++ src/main/java/info/ponciano/lab/geotimewfs/controllers/examples/semanticwfs/QueryinterfaceController.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2020 Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package info.ponciano.lab.geotimewfs.controllers.examples.semanticwfs;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *Controller of queryinterface
+ * @author Dr Jean-Jacques Ponciano <jean-jacques@ponciano.info>
+ */
+@Controller
+public class QueryinterfaceController {
+           @GetMapping("/queryinterface")
+    public String greeting(@RequestParam(name = "name",
+            required = false,
+            defaultValue = "name")
+            String sd,
+           Model model) {
+        model.addAttribute("name", sd);
+        return "/semanticwfs/queryinterface";
+    }
+}
