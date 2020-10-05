e0b250729f52c06f485150773f8385e9ef3748d8
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-10-05 10:25:29 +0200

    Create GeotimewfsApplication.java

---
+++ src/main/java/info/ponciano/lab/geotimewfs/GeotimewfsApplication.java
@@ -0,0 +1,13 @@
+package info.ponciano.lab.geotimewfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeotimewfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeotimewfsApplication.class, args);
+	}
+
+}
