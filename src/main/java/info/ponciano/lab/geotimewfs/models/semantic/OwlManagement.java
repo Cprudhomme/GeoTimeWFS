78de6a49537dc33e4ce98f430d77392784ebc8f5
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-10-15 17:57:18 +0200

    Update OwlManagement.java
    first test to load xml file

--- src/main/java/info/ponciano/lab/geotimewfs/models/semantic/OwlManagement.java
+++ src/main/java/info/ponciano/lab/geotimewfs/models/semantic/OwlManagement.java
@@ -17,11 +17,67 @@
  * MA 02110-1301  USA
  */
 package info.ponciano.lab.geotimewfs.models.semantic;
-
+import java.io.File;  
+import javax.xml.parsers.DocumentBuilder;  
+import javax.xml.parsers.DocumentBuilderFactory;  
+import org.w3c.dom.Document;  
+import org.w3c.dom.NamedNodeMap;  
+import org.w3c.dom.Node;  
+import org.w3c.dom.NodeList;
 /**
  *
  * @author Dr Jean-Jacques Ponciano Contact: jean-jacques@ponciano.info
  */
-public class OwlManagement {
-    
+public class OwlManagement extends OntoManagement {
+
+    @Override
+    public boolean uplift(String xmlPathfile) {
+        try {
+            File file = new File(xmlPathfile);
+            DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document document = documentBuilder.parse(file);
+            System.out.println("Root element: " + document.getDocumentElement().getNodeName());
+            if (document.hasChildNodes()) {
+                printNodeList(document.getChildNodes());
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    private static void printNodeList(NodeList nodeList) {
+        for (int count = 0; count < nodeList.getLength(); count++) {
+            Node elemNode = nodeList.item(count);
+            if (elemNode.getNodeType() == Node.ELEMENT_NODE) {
+// get node name and value  
+                System.out.println("\nNode Name =" + elemNode.getNodeName() + " [OPEN]");
+                System.out.println("Node Content =" + elemNode.getTextContent());
+                if (elemNode.hasAttributes()) {
+                    NamedNodeMap nodeMap = elemNode.getAttributes();
+                    for (int i = 0; i < nodeMap.getLength(); i++) {
+                        Node node = nodeMap.item(i);
+                        System.out.println("attr name : " + node.getNodeName());
+                        System.out.println("attr value : " + node.getNodeValue());
+                    }
+                }
+                if (elemNode.hasChildNodes()) {
+//recursive call if the node has child nodes  
+                    printNodeList(elemNode.getChildNodes());
+                }
+                System.out.println("Node Name =" + elemNode.getNodeName() + " [CLOSE]");
+            }
+        }
+    }
+
+    @Override
+    public boolean change(String... param) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getSPARQL(String... param) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
