3bf47936545a2fc53b34b4d4cb82bfc072441997
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2020-10-16 22:01:41 +0200

    Update OwlManagement.java

--- src/main/java/info/ponciano/lab/geotimewfs/models/semantic/OwlManagement.java
+++ src/main/java/info/ponciano/lab/geotimewfs/models/semantic/OwlManagement.java
@@ -21,17 +21,28 @@ package info.ponciano.lab.geotimewfs.models.semantic;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import org.apache.jena.ontology.Individual;
 import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntProperty;
 import org.apache.jena.ontology.OntResource;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.StmtIterator;
 import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -59,10 +70,11 @@ public class OwlManagement extends OntoManagement {
             File file = new File(xmlPathfile);
             DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
             Document document = documentBuilder.parse(file);
-            System.out.println("Root element: " + document.getDocumentElement().getNodeName());
             if (document.hasChildNodes()) {
                 NodeList childNodes = document.getChildNodes();
                 writeNodeList(childNodes.item(0).getChildNodes(), null, null);
+            } else {
+                return false;
             }
         } catch (OntoManagementException | IOException | ParserConfigurationException | SAXException e) {
             Logger.getLogger(OwlManagement.class.getName()).log(Level.SEVERE, null, e);
@@ -148,7 +160,7 @@ public class OwlManagement extends OntoManagement {
                 String attrName1 = node.getNodeName();
                 String attrValue = node.getNodeValue();
                 if (attrName1.equals("codeListValue") || attrName1.equals("codeListElementValue")) {
-                    n = this.ont.getIndividual(NS + attrValue);
+                    n = this.ont.getIndividual(NS + attrValue);//here the ontology load "dataset" which is a property instead of "_dataset" which is an individual.
                     notCreate = false;
                 } else if (attrName1.toLowerCase().equals("uuid")) {
                     n = this.ont.createIndividual(NS + attrValue, nodeClass);
@@ -179,6 +191,68 @@ public class OwlManagement extends OntoManagement {
 
     @Override
     public String downlift(String metadataURI) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document document = documentBuilder.newDocument();
+            //get metadata individual
+            Individual individual = this.ont.getIndividual(metadataURI);
+
+            Element rootElement = document.createElement(individual.getLocalName());
+            document.appendChild(rootElement);
+
+            recDownlift(individual, document, rootElement);
+            // create the xml file
+            // output DOM XML to console 
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            DOMSource source = new DOMSource(document);
+            StringWriter buff = new StringWriter();
+            StreamResult console = new StreamResult(buff);
+            transformer.transform(source, console);
+            return buff.toString();
+        } catch (ParserConfigurationException | TransformerException ex) {
+            Logger.getLogger(OwlManagement.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "";
     }
+
+    private void recDownlift(Individual individual, Document document, Element rootElement) throws DOMException {
+        System.out.println("Individual: "+individual);
+        if (individual != null) {
+            //add this individual to rootElement
+            Element current = document.createElement(individual.getOntClass().getLocalName());
+            rootElement.appendChild(current);
+            //get properties
+            StmtIterator listProperties = individual.listProperties();
+            while (listProperties.hasNext()) {
+                Statement next = listProperties.next();
+                String nameP = next.getPredicate().getURI();
+                OntProperty predicate = this.ont.getOntProperty(nameP);
+                RDFNode object = next.getObject();
+                System.out.println("Object: "+object);
+                System.out.println("P: "+predicate);
+                if (predicate == null || !OwlManagement.containsNS(predicate.getNameSpace())) {
+                    System.out.println(nameP + " skiped");
+                } else //if it is an object property
+                if (predicate.isObjectProperty()) {
+                    //add the property to the element and recusively go to the object property
+                    Element predicatElement = document.createElement(predicate.getLocalName());
+                    current.appendChild(predicatElement);
+                    //get object as resource
+                    Individual iobj = this.ont.getIndividual(object.asResource().getURI());
+                    recDownlift(iobj, document, predicatElement);
+                } else // IF it is a data property
+                if (predicate.isDatatypeProperty()) {
+                    Element e = document.createElement(predicate.getLocalName());
+                    current.appendChild(e);
+
+                    Element se = document.createElement("CharacterString");
+                    se.appendChild(document.createTextNode(object.toString()));
+                    e.appendChild(se);
+                }
+
+            }
+        }
+    }
+
 }
