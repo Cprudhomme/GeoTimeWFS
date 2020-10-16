4468a0833679c670bc6d39b9b764bdea4e7d9e36
Claire <prudh.claire@gmail.com>
2020-10-16 19:29:52 +0200

    Successful integration of file loading

--- src/main/java/info/ponciano/lab/geotimewfs/controllers/MetadataController.java
+++ src/main/java/info/ponciano/lab/geotimewfs/controllers/MetadataController.java
@@ -1,35 +1,74 @@
 package info.ponciano.lab.geotimewfs.controllers;
 
+import info.ponciano.lab.geotimewfs.controllers.storage.StorageFileNotFoundException;
+import info.ponciano.lab.geotimewfs.controllers.storage.StorageService;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MetadataController {
+
+    private final StorageService storageService;
+
+    @Autowired
+    public MetadataController(StorageService storageService) {
+        this.storageService = storageService;
+    }
 
     /* 
     parameter not yet defined 
      */
     @GetMapping("/metadata/uplift")
-    public String getUpliftView(@RequestParam(name = "name", required = false, defaultValue = "World") String name) {
+    public String getUpliftView(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("files", storageService.loadAll().map(
+				path -> MvcUriComponentsBuilder.fromMethodName(MetadataController.class,
+						"serveFile", path.getFileName().toString()).build().toUri().toString())
+				.collect(Collectors.toList()));
         return "upliftView";
     }
+    
+    @GetMapping("/metadata/uplift/files/{filename:.+}")
+	@ResponseBody
+	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+
+		Resource file = storageService.loadAsResource(filename);
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+				"attachment; filename=\"" + file.getFilename() + "\"").body(file);
+	}
 
     /* 
     parameter not yet defined 
      */
     @PostMapping("/metadata/uplift")
-    public String postUpliftAction(@RequestParam(name = "name", required = false, defaultValue = "World") String name) {
-        //download the xml file
-        //save it a folder "public"
-        //call uplif forn the owlManagement model
+    public String postUpliftAction(@RequestParam("file") MultipartFile file,
+			RedirectAttributes redirectAttributes) {
 
-        //redirection
-        return "redirect:/metadata";
+		storageService.store(file);
+		redirectAttributes.addFlashAttribute("message",
+				"You successfully uploaded " + file.getOriginalFilename() + "!");
+        //return "redirect:/metadata";
+        return "redirect:/metadata/uplift";
     }
 
+    @ExceptionHandler(StorageFileNotFoundException.class)
+	public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+		return ResponseEntity.notFound().build();
+	}
+        
     /* 
     parameter not yet defined 
      */
---
+++ src/test/java/info/ponciano/lab/geotimewfs/controllers/MetadataControllerTest.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright (C) 2020 claireprudhomme.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package info.ponciano.lab.geotimewfs.controllers;
+
+import info.ponciano.lab.geotimewfs.controllers.storage.StorageFileNotFoundException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+/**
+ *
+ * @author claireprudhomme
+ */
+public class MetadataControllerTest {
+    
+    public MetadataControllerTest() {
+    }
+
+    /**
+     * Test of getUpliftView method, of class MetadataController.
+     */
+    @Test
+    public void testGetUpliftView() {
+        System.out.println("getUpliftView");
+        String name = "";
+        Model model = null;
+        MetadataController instance = null;
+        String expResult = "";
+        String result = instance.getUpliftView(name, model);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of serveFile method, of class MetadataController.
+     */
+    @Test
+    public void testServeFile() {
+        System.out.println("serveFile");
+        String filename = "";
+        MetadataController instance = null;
+        ResponseEntity<Resource> expResult = null;
+        ResponseEntity<Resource> result = instance.serveFile(filename);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of postUpliftAction method, of class MetadataController.
+     */
+    @Test
+    public void testPostUpliftAction() {
+        System.out.println("postUpliftAction");
+        MultipartFile file = null;
+        RedirectAttributes redirectAttributes = null;
+        MetadataController instance = null;
+        String expResult = "";
+        String result = instance.postUpliftAction(file, redirectAttributes);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of handleStorageFileNotFound method, of class MetadataController.
+     */
+    @Test
+    public void testHandleStorageFileNotFound() {
+        System.out.println("handleStorageFileNotFound");
+        StorageFileNotFoundException exc = null;
+        MetadataController instance = null;
+        ResponseEntity expResult = null;
+        ResponseEntity result = instance.handleStorageFileNotFound(exc);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getMetadata method, of class MetadataController.
+     */
+    @Test
+    public void testGetMetadata() {
+        System.out.println("getMetadata");
+        String name = "";
+        MetadataController instance = null;
+        String expResult = "";
+        String result = instance.getMetadata(name);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getSelectedMd method, of class MetadataController.
+     */
+    @Test
+    public void testGetSelectedMd() {
+        System.out.println("getSelectedMd");
+        String name = "";
+        MetadataController instance = null;
+        String expResult = "";
+        String result = instance.getSelectedMd(name);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getMdChangeView method, of class MetadataController.
+     */
+    @Test
+    public void testGetMdChangeView() {
+        System.out.println("getMdChangeView");
+        String name = "";
+        MetadataController instance = null;
+        String expResult = "";
+        String result = instance.getMdChangeView(name);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
