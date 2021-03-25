5b94cdad806161839ad2906b21232e3e5842643c
Claire <prudh.claire@gmail.com>
2021-03-25 20:37:11 +0100

    csv loading implementation

--- src/main/java/info/ponciano/lab/array_uplift/ArrayUpliftController.java
+++ src/main/java/info/ponciano/lab/array_uplift/ArrayUpliftController.java
@@ -1,37 +1,106 @@
 package info.ponciano.lab.array_uplift;
 
+import java.io.IOException;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+import info.ponciano.lab.geotimewfs.controllers.storage.StorageService;
+import info.ponciano.lab.geotimewfs.models.SHPdata;
+import info.ponciano.lab.pitools.files.PiFile;
 
 @Controller
-public abstract class ArrayUpliftController {
+public class ArrayUpliftController {
 	
 	private ArrayUpliftModel am;
+	private final StorageService storageService;
+
+    @Autowired
+    public ArrayUpliftController(StorageService storageService) {
+        this.storageService = storageService;
+    }
 
 	//view to load CSV file
 	@GetMapping("/csv_loading")
-    public abstract String getCsvLoadingView(Model model);
+    public String getCsvLoadingView(Model model) {
+		model.addAttribute("files", storageService.loadAll().map(
+                path -> MvcUriComponentsBuilder.fromMethodName(ArrayUpliftController.class,
+                        "serveFile", path.getFileName().toString()).build().toUri().toString())
+                .collect(Collectors.toList()));
+        return "csvloading";
+    }
+
+    @GetMapping("/csv_loading/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
 	
 	//view to load DBF file
-	@GetMapping("/dbf_loading")
-    public abstract String getDbfLoadingView(Model model);
+	//@GetMapping("/dbf_loading")
+    //public abstract String getDbfLoadingView(Model model);
 	
 	//View with name of uploaded file and view of the array to define the properties of the ontology  
-	@GetMapping("/array_uplift")
-    public abstract String getArrayUpliftView(Model model);
+	@PostMapping("/array_uplift")
+    public String arrayUpliftView(@RequestParam("file") MultipartFile file, Model model) {//RedirectAttributes redirectAttributes) {
+		// store file
+		storageService.store(file);
+
+		//redirectAttributes.addFlashAttribute("message",
+						//"You successfully upload the file(s): " + file.getOriginalFilename() 
+						//+ ". Please indicate now the mapping between attributes of your file and the appropriate ontological properties");
+		
+		// store the csv file in a specific folder for CSV files
+		String pathCSV = "csv-dir/" + file.getOriginalFilename();
+		try {
+			moveFile(file.getOriginalFilename(), pathCSV);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String message="You successfully upload the file(s): " + file.getOriginalFilename() ;
+		message += ". Please indicate now the mapping between attributes of your file and the appropriate ontological properties";
+		model.addAttribute("message", message);
+		return "arrayMappingView";
+		
+	}
+	
+	/**
+	 * move a file from a directory to the CSV storage directory
+	 * 
+	 * @param filename: name of the file to move
+	 * @param pathSHP:  initial path of the file to move
+	 * @throws IOException
+	 */
+	private void moveFile(String filename, String pathSHP) throws IOException {
+		PiFile dirSHPdata = new PiFile(SHPdata.DIR_SHP_DATA);
+		if (!dirSHPdata.exists())
+			dirSHPdata.mkdir();
+		new PiFile(pathSHP).mv(dirSHPdata.getPath() + filename);
+	}
 	
 	//adding of a new Property from its local name, range and type
-	//require to update 
-	@PostMapping("/property_adding")
-    public abstract String addNewProperty();
+	//require to update hashmap and list 
+	//@PostMapping("/property_adding")
+    //public abstract String addNewProperty();
 	
-	@PostMapping("/uplift_validation")
-    public abstract String ontologyPopulation();
+	//@PostMapping("/uplift_validation")
+    //public abstract String ontologyPopulation();
 	
 	
 	
--- src/main/java/info/ponciano/lab/array_uplift/ArrayUpliftModel.java
+++ src/main/java/info/ponciano/lab/array_uplift/ArrayUpliftModel.java
@@ -5,22 +5,34 @@ import java.util.HashMap;
 import java.util.List;
 
 import info.ponciano.lab.pisemantic.PiOnt;
+import info.ponciano.lab.pisemantic.PiSparql;
 
 public abstract class ArrayUpliftModel {
 
-	//table representing a data to uplift as RDF triples
-	private String [][] table;
+	/**
+	 * table representing a data to uplift as RDF triples
+	 */
+	protected String [][] attributes;
+	/**
+	 * path of the ontology to create from attributes
+	 */
+	protected String ontopath;
 	//Fixed path of the vocabulary ontology
-	private static final String VOCAB_PATH="";
+	protected static final String VOCAB_PATH="";
 	//Ontology containing the vocabulary/TBox for the ontology representing a table data
-	private PiOnt vocab;
+	protected PiOnt vocab;
+	//Ontology to create from attributes
+	protected PiSparql ontology;
 	//Hashmap having name in table as key and property URI as corresponding value
-	private HashMap<String, String> propertyNames;
+	protected HashMap<String, String> propertyNames;
 	//Hashmap having property URI as key and its corresponding range as value
-	private HashMap<String, String> propertyRanges;
+	protected HashMap<String, String> propertyRanges;
 	
-	public ArrayUpliftModel (String [][] attributes) throws FileNotFoundException {
-		this.table=attributes;
+	
+	public ArrayUpliftModel (String [][] attributesArray, String ontpath) throws FileNotFoundException {
+		this.attributes=attributesArray;
+		this.ontopath=ontpath;
+		this.ontology= new PiSparql(this.ontopath);
 		//can make a PiFile to verify if file exists, and create it if not.
 		this.vocab= new PiOnt(VOCAB_PATH);
 		this.initpropertyNames();
@@ -34,5 +46,19 @@ public abstract class ArrayUpliftModel {
 	public abstract List<String> getObjectProperties();
 	public abstract List<String> getDataProperties();
 	
+	/**
+	 * adding of a new Property from its local name, range and type that update hashmap and list of properties
+	 * @param localname of the property to add
+	 * @param range of the property to add, which allows also to determine if it is an object property or a data property according to if the range contains "xsd" at the beginning (to manage in web interface)
+	 * @return validation of the property adding
+	 */
+	public abstract boolean addProperty(String localname, String range);
 	
+	
+	public abstract boolean createOntology();
+	/**
+	 * 
+	 * @return the created ontology from attributes
+	 */
+	public abstract PiSparql getOntology(); 
 }
