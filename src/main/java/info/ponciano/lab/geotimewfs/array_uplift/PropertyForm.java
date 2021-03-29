ea6400bf320e265adb8dcd650f68bf56e3ac3ea5
Jean-Jacques Ponciano <ponciano.jeanjacques@gmail.com>
2021-03-29 13:26:19 +0200

    Create PropertyForm.java

---
+++ src/main/java/info/ponciano/lab/geotimewfs/array_uplift/PropertyForm.java
@@ -0,0 +1,43 @@
+package info.ponciano.lab.geotimewfs.array_uplift;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class  PropertyForm  {
+
+  @NotNull
+	@Size(min=2, max=30)
+	private String name;
+
+  @NotNull
+	@Size(min=2, max=30)
+	private String range;
+
+
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+/**
+ * @param range the range to set
+ */
+public void setRange(String range) {
+	this.range = range;
+}
+/**
+ * @return the range
+ */
+public String getRange() {
+	return range;
+}
+@Override
+public String toString() {
+  return 	 "Property( Name: " + this.name + ", Age: " + this.range + ")";
+
+}
+}
