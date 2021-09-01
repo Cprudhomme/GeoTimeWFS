40fa58107d7ffba1fb09c18c44952ed8d180fcc1
AHafsaoui <88088032+AHafsaoui@users.noreply.github.com>
2021-09-01 14:21:55 +0200

    Update CountupTest.java
    Line 56

--- src/test/java/info/pociano/lab/time_count/CountupTest.java
+++ src/test/java/info/pociano/lab/time_count/CountupTest.java
@@ -53,7 +53,7 @@ public class CountupTest {
     @Test
     public void testSomeMethod() {
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        fail("The test case is a prototype."); //Line Error
     }
     
 }
