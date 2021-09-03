d054264a9d4df419837a393e971111b324473064
AHafsaoui <88088032+AHafsaoui@users.noreply.github.com>
2021-09-03 09:04:05 +0200

    Update Time.java

--- src/main/java/info/pociano/lab/time_count/Time.java
+++ src/main/java/info/pociano/lab/time_count/Time.java
@@ -18,11 +18,22 @@
  */
 package info.pociano.lab.time_count;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Formatter;
+
 /**
  *
  * @author jean-jacques ponciano
  */
 public class Time {
+
+    /**
+     *
+     * @param args
+     */
+    
 
     public static void main(String[] args) {
         String now = Time.now();
@@ -37,23 +48,59 @@ public class Time {
         System.out.println(weeks);
     }
 
-    public static int daysBetween(String d1, String d2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static int daysBetween (String d1, String d2)  {
+            DateTimeFormatter Formatter = DateTimeFormatter.ofPattern("dd/MM/uuuu");
+           
+       
+        
+        
+        LocalDate ld1  = LocalDate.parse(d1, Formatter);
+         LocalDate ld2 = LocalDate.parse(d2, Formatter);
+         
+          long daysBetween = ChronoUnit.DAYS.between(ld1,ld2);
+         
+         System.out.println(daysBetween);
+        return (int) daysBetween;
+              
     }
-
+     
     public static int yearsBetween(String d1, String d2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        DateTimeFormatter Formatter = DateTimeFormatter.ofPattern("dd/MM/uuuu");
+    
+        String date1 = "31-12-2021";
+        String date2 = "12-12-2012";
+        
+        LocalDate date1 = LocalDate.parse(date1, Formatter);
+         LocalDate date2 = LocalDate.parse(date2, Formatter);
+         
+         long monthsBetween = ChronoUnit.YEARS.between(date1,date2);
+         
+         System.out.println(Integer.parseInt(monthsBetween);
+                
+     
     }
 
     public static int monthsBetween(String d1, String d2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         DateTimeFormatter Formatter = DateTimeFormatter.ofPattern("dd/MM/uuuu");
+    
+
+        String date1 = "31-12-2021";
+        String date2 = "12-12-2012";
+        
+        LocalDate date1 = LocalDate.parse(date1, Formatter);
+         LocalDate date2 = LocalDate.parse(date2, Formatter);
+         
+         long monthsBetween = ChronoUnit.MONTHS.between(date1,date2);
+         
+         System.out.println(Integer.parseInt(monthsBetween);
+                
     }
 
     public static String now() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+     
     }
 
-    public static int weeksBetween(String d1, String f2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static int weeksBetween(String d1, String d2) {
+        
     }
 }
